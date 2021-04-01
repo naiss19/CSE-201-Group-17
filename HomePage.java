98f877b62778607e91a9ade72f4be8e8fed7353e
Brandon Hall <48291194+naiss19@users.noreply.github.com>
2021-04-01 15:02:35 -0400

    Changed the sort JMenu to a JComboBox

--- HomePage.java
+++ HomePage.java
@@ -21,23 +21,11 @@ import javax.swing.JTextField;
  * @author BrandonHall
  *
  */
-public class HomePage extends JPanel implements ActionListener{
-	//public static Event[] eventList = new Event[1]; 
-	/**
-	 * 
-	 * Inner class that is used to test the UI and eventList iteration.
-	 *
-	 */
-	private class Event {
-		String name, type, description, phoneNum, link;
-		Event() {
-			this.name = "Brick Street";
-			this.type = "Night Life";
-			this.description = "The biggest bar in uptown Oxford";
-			this.phoneNum = "614-672-2863";
-			this.link = "www.brickstreetbar.com";
-		}
-	}
+public class HomePage extends JPanel implements ActionListener {
+	//TODO: Change sort into a JComboBox
+	String[] sortTypes = {"Name",  "Type", "Price", "Location", "Rating"};
+	JComboBox sort = new JComboBox(sortTypes);
+	JButton go = new JButton("Go!");
 	/**
 	 * Creates the home page using a couple JPanels, a JButton, and a JMenu to sort by a certain criteria
 	 * @return the home page jpanel
@@ -61,16 +49,9 @@ public class HomePage extends JPanel implements ActionListener{
 		label.setFont (label.getFont ().deriveFont (64.0f));
 		label.setFont(new Font("Times New Roman", Font.BOLD, 64));
 		
-		JButton go = new JButton("Go!");
 		
-		//Creating the JMenu and adding JMenuItems for the sort menu
-		JMenu sort = new JMenu("Sort by:");
-		JMenuItem m1 = new JMenuItem("Name");
-		JMenuItem m2 = new JMenuItem("Type");
-		JMenuItem m3 = new JMenuItem("Price");
-		JMenuItem m4 = new JMenuItem("Location");
-		JMenuItem m5 = new JMenuItem("Rating");
-		sort.add(m1); sort.add(m2);	sort.add(m3); sort.add(m4);	sort.add(m5);
+		
+				
 		sort.addActionListener(this);
 		
 		//Adding all of the components to the internal panels
@@ -98,10 +79,11 @@ public class HomePage extends JPanel implements ActionListener{
 	 * @param args main parameter for the main method
 	 */
 	public static void main(String[] args) {
-		HomePage p = new HomePage();
+		HomePage home = new HomePage();
 		JFrame mainFrame = new JFrame();
-		JPanel mainPanel = new JPanel(new BorderLayout());
-		mainPanel.add(p.searchBar());
+		JPanel mainPanel = new JPanel();
+		mainPanel.add(home.searchBar());
+		
 		mainFrame.setSize(1000, 600);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.add(mainPanel);
@@ -115,6 +97,9 @@ public class HomePage extends JPanel implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		if (e.getSource() == go) {
+			
+		}
 		
 	}
 }
