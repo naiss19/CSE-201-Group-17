1dd828ee0482882d6ebc21d21a56535bd23fd476
Brandon Hall <48291194+naiss19@users.noreply.github.com>
2021-04-16 16:04:39 -0400

    Added image resizing

--- EventPanelOOP2.java
+++ EventPanelOOP2.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
@@ -190,6 +192,7 @@ public class EventPanelOOP2 extends JFrame{
 		try {
 			url = new URL(path);
 			image = ImageIO.read(url);
+			image = resize(image, 400, 3 00);
 		} catch (IOException exception) {
 		}
 
@@ -199,4 +202,15 @@ public class EventPanelOOP2 extends JFrame{
 		return label;
 
 	}
+	
+	public static BufferedImage resize(BufferedImage img, int newW, int newH) { 
+	    Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+	    BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+	    Graphics2D g2d = dimg.createGraphics();
+	    g2d.drawImage(tmp, 0, 0, null);
+	    g2d.dispose();
+
+	    return dimg;
+	}  
 }
