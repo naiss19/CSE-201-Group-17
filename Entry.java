eda68db642ef6b853356d1ae563a61146572457c
hannahahlstrom <64744941+hannahahlstrom@users.noreply.github.com>
2021-04-21 16:19:20 -0400

    Update Entry.java

--- Entry.java
+++ Entry.java
@@ -14,7 +14,7 @@ import javax.swing.JPanel;
 import javax.swing.border.Border;
 
 /**
- * This is the Entry class that creates entry events to store the data for each eventPanel display.
+ * This is the Entry class that creates entry events to store the data for the Catalogue event display.
  * @author Zach Katz, Brandon Hall, Hannah Ahlstrom
  * 
  */
